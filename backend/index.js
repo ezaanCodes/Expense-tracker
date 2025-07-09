3c340469c4d30d8a24f3ff849f5ba3eb42fb9c57
ezaanCodes <izaansatti@gmail.com>
2025-07-09 22:16:48 +0500

    Initial clean commit

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import cors from "cors";
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const app = express();
+
+// app.use(cors("*"))
+app.use(cors());
+
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+// app.use("/api-v1", (req, res) => {
+//     res.json({ message: "API v1 root" });
+// })
+
+app.use((req, res) => {
+    res.status(401).json({
+        status: "404 Not Found",
+        message: "Resource/Route not found"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
+
+// _ -
---
+++ backend/libs/database.js
@@ -0,0 +1,9 @@
+import pg from "pg"
+import decrypt from "dotenv"
+
+dotenv.config()
+
+const { Pool } = pg;
+export const pool = new Pool ({
+    connectionString: process.env.DATABASE_URI,
+})
