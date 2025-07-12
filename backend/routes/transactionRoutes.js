94a54a2a3b32d4e537979cc9027efb2eb6fe1237
ezaanCodes <izaansatti@gmail.com>
2025-07-12 22:32:54 +0500

    transactions routes and controllers created

---
+++ backend/routes/transactionRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import authMiddleware from "../middleware/authMiddleware.js"
+import { addTransaction, getDashboardInformation, getTransactions, transferMoneyToAccount } from "../controllers/transactionController.js"
+
+const router = express.Router()
+
+router.get("/", authMiddleware, getTransactions)
+router.get("/dashboard", authMiddleware, getDashboardInformation)
+router.post("/add-transaction/:accountid", authMiddleware, addTransaction)
+router.put("/transfer-money", authMiddleware, transferMoneyToAccount)
+
+
+export default router
