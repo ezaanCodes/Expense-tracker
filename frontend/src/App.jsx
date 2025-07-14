6393f107b6be1c838911ca93b6318ab61596d95b
ezaanCodes <izaansatti@gmail.com>
2025-07-14 22:18:42 +0500

    Routes Setup for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,51 @@
+import { Navigate, Outlet, Route, Routes } from "react-router-dom"
+import SignIn from "./pages/auth/sign-in"
+import SignUp from "./pages/auth/sign-up"
+import Dashboard from "./pages/dashboard"
+import Transactions from "./pages/transactions"
+import Settings from "./pages/settings"
+import Account from "./pages/account-page"
+
+const RootLayout = () => {
+  const user = null
+
+  return !user ? <Navigate to="/sign-up" /> : (
+
+    <>
+    {/* <Navbar/> */}
+      <div>
+        <Outlet />
+      </div>
+    </>
+
+  )
+}
+
 function App() {
 
   return (
-    <>
-      <h1>Hello world </h1>
+    <div>
+      <main>
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route path="/" element={<Navigate to="overview" />} />
+            <Route path="/overview" element={<Dashboard />} />
+            <Route path="/transactions" element={<Transactions />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<Account />} />
 
-    </>
+
+          </Route>
+          <Route path="/sign-up" element={SignUp} />
+          <Route path="/sign-in" element={SignIn} />
+
+        </Routes>
+
+      </main>
+
+    </div>
   )
 }
 
 export default App
+// _-|
---
+++ frontend/src/pages/auth/sign-up.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignUp = () => {
+    return (
+        <div ><h1 className='text-black'>SignUpkajsndkjasndkjasnd</h1></div>
+    )
+}
+
+export default SignUp
