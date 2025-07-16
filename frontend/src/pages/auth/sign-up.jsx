16e0a199305e3d398e717bb29383986b5244f67e
ezaanCodes <izaansatti@gmail.com>
2025-07-16 19:34:54 +0500

    zod Register schema created

--- frontend/src/pages/auth/sign-up.jsx
+++ frontend/src/pages/auth/sign-up.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import * as z from "zod";
+
+const RegisterSchema = z.object({
+    email: z
+        .string({ required_error: "Email is required" })
+        .email({message: "Invalid email address"}),
+    firstName: z.string({required_error: "Name is required"}),
+    password:z
+        .string({required_error: "Name is required"})
+        .min(1, "Password is required")
+});
 
 const SignUp = () => {
+
+
     return (
-        <div ><h1 className='text-black'>SignUpkajsndkjasndkjasnd</h1></div>
+        <div >
+            <h1 className='text-black'>SignUpkajsndkjasndkjasnd</h1>
+        </div>
     )
 }
 
