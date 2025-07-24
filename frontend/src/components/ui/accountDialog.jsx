a190ba308eeb3524219b4c5a3c05e7404c5dcfa4
ezaanCodes <izaansatti@gmail.com>
2025-07-25 00:13:54 +0500

    created account dialog

---
+++ frontend/src/components/ui/accountDialog.jsx
@@ -0,0 +1,44 @@
+import { MdMoreVert } from "react-icons/md";
+import TransitionWrapper from "./wrappers/transition";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { BiTransfer } from "react-icons/bi";
+export default function AccountMenu({ addMoney, transferMoney }) {
+  return (
+    <>
+      <Menu as="div" className="relative inline-block text-left">
+        <MenuButton className="inline-flex w-full justify-center rounded-md text-sm font-medium text-gray-600 dark:text-gray-300">
+          <MdMoreVert />
+        </MenuButton>
+
+        <TransitionWrapper>
+          <MenuItems className="absolute p-2 right-0 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg dark:bg-gray-900">
+            <div className="px-1 py-1 space-y-2">
+              <MenuItem>
+                {() => (
+                  <button
+                    onClick={transferMoney}
+                    className="group flex gap-2 w-full items-center rounded-md px-2 py-2 text-sm text-gray-700 dark:text-gray-200"
+                  >
+                    <BiTransfer />
+                    Transfer Funds
+                  </button>
+                )}
+              </MenuItem>
+              <MenuItem>
+                {() => (
+                  <button
+                    onClick={addMoney}
+                    className="group flex gap-2 w-full items-center rounded-md px-2 py-2 text-sm text-gray-700 dark:text-gray-200"
+                  >
+                    <BiTransfer />
+                    Add Funds
+                  </button>
+                )}
+              </MenuItem>
+            </div>
+          </MenuItems>
+        </TransitionWrapper>
+      </Menu>
+    </>
+  );
+}
