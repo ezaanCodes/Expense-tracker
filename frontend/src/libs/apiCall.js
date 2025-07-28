3ee4b23e71db0afbed9aa951a60dad710f8d6f05
ezaanCodes <izaansatti@gmail.com>
2025-07-28 17:27:33 +0500

    App URL updated from localost to render

--- frontend/src/libs/apiCall.js
+++ frontend/src/libs/apiCall.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-const API_URL= 'http://localhost:5000/api-v1'
+// const API_URL= 'http://localhost:5000/api-v1'
+const API_URL= 'https://expense-tracker-rsit.onrender.com/api-v1'
 
 const api = axios.create({
     baseURL: API_URL,
